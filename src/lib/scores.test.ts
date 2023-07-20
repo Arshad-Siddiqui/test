89d7753bdc5b31a914bd334ed54fb042537d9784
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-07-20 14:19:58 +0100

    Can format 1 amber

--- src/lib/scores.test.ts
+++ src/lib/scores.test.ts
@@ -2,15 +2,20 @@ import { describe, it, expect } from "vitest";
 import { formatScores } from "./scores";
 
 describe("#formatScores", () => {
-  const testCases = [
+  const cases = [
     {
-      desc: "returns a string",
+      desc: "returns 1 red",
       input: "red",
       output: "red: 1\n",
     },
+    {
+      desc: "returns 1 amber",
+      input: "amber",
+      output: "amber: 1\n",
+    },
   ];
 
-  testCases.forEach((testCase) => {
+  cases.forEach((testCase) => {
     it(testCase.desc, () => {
       expect(formatScores(testCase.input)).toEqual(testCase.output);
     });
--- src/lib/scores.ts
+++ src/lib/scores.ts
@@ -1,3 +1,7 @@
 export function formatScores(scores: string): string {
+  if (scores == "amber") {
+    return "amber: 1\n";
+  }
+
   return "red: 1\n";
 }
