0f46d2587f88d3ebfedbb4cc4a3e39c434ca4cb7
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-07-25 11:42:35 +0100

    Refactor with modulus and add 0 check

--- src/lib/fizzbuzz.ts
+++ src/lib/fizzbuzz.ts
@@ -1,9 +1,13 @@
 export default function fizzbuzz(number: number): number | string {
-  if (number == 3 || number == 6) {
+  if (number == 0) {
+    return 0;
+  }
+
+  if (number % 3 == 0) {
     return "fizz";
   }
 
-  if (number == 5) {
+  if (number % 5 == 0) {
     return "buzz";
   }
 
