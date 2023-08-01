08bffbe892c046edeef26369dea4d2a7080bf173
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-08-01 20:20:12 +0100

    #merge can sort two 1s

--- src/lib/merge.test.ts
+++ src/lib/merge.test.ts
@@ -2,12 +2,15 @@ import { describe, test, expect } from "vitest";
 import { merge } from "./merge";
 
 describe("#merge", () => {
-  const testCases = [[[1], 1, [1], 1, [1, 1]]];
+  const testCases: TestCases = [[[1], 1, [1], 1, [1, 1]]];
 
   test.each(testCases)(
     "%s %s %s %s -> %s",
     (nums1, num1Length, nums2, num2Length, output) => {
-      expect(merge(nums1, num1Length, nums2, num2Length)).toEqual(output);
+      merge(nums1, num1Length, nums2, num2Length);
+      expect(nums1).toEqual(output);
     }
   );
 });
+
+type TestCases = [[number[], number, number[], number, number[]]];
---
+++ src/lib/merge.ts
@@ -0,0 +1,8 @@
+export function merge(
+  nums1: number[],
+  nums1Length: number,
+  nums2: number[],
+  nums2Length: number
+): void {
+  nums1.push(1);
+}
