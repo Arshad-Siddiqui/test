eba664f630d25f134c9f304e4226227127058186
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-08-01 21:08:32 +0100

    Only removes zeroes if outside specified length

--- src/lib/merge.test.ts
+++ src/lib/merge.test.ts
@@ -5,10 +5,14 @@ describe("#merge", () => {
   const testCases: TestCases = [
     [[1, 0], 1, [1], 1, [1, 1]],
     [[1, 0], 1, [2], 1, [1, 2]],
+    [[2, 0], 1, [1], 1, [1, 2]],
+    [[4, 5, 6, 0, 0, 0], 3, [1, 2, 2], 3, [1, 2, 2, 4, 5, 6]],
+    [[-1, 0, 1, 0, 0], 3, [3, 5], 2, [-1, 0, 1, 3, 5]],
+    [[-1, -1, 0, 0, 0, 0], 4, [-1, 0], 2, [-1, -1, -1, 0, 0, 0]],
   ];
 
   test.each(testCases)(
-    "%s %s %s %s -> %s",
+    "%s of length %s and %s of length %s -> %s",
     (nums1, num1Length, nums2, num2Length, output) => {
       merge(nums1, num1Length, nums2, num2Length);
       expect(nums1).toEqual(output);
--- src/lib/merge.ts
+++ src/lib/merge.ts
@@ -1,9 +1,12 @@
 export function merge(
   nums1: number[],
-  nums1Length: number,
+  m: number,
   nums2: number[],
-  nums2Length: number
+  n: number
 ): void {
-  nums1.pop();
-  nums1.push(nums2[0]);
+  nums1.splice(
+    0,
+    nums1.length,
+    ...[...nums1.slice(0, m), ...nums2].sort((a, b) => a - b)
+  );
 }
