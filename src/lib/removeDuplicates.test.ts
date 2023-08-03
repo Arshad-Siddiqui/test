1ceaad8f31639240adb5c35ef3c5b69b1a015c95
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-08-03 10:40:08 +0100

    Can convert [1, 1] -> [1] and return 1

--- src/lib/removeDuplicates.test.ts
+++ src/lib/removeDuplicates.test.ts
@@ -1,15 +1,43 @@
 import { describe, test, expect } from "vitest";
 import { removeDuplicates } from "./removeDuplicates";
 
-describe("#removeDuplicates", () => {
-  describe("Can return arrays of 1 length", () => {
-    const testCases: TestCases = [[[1], [1], 0]];
-
-    test.each(testCases)("%s -> %output", (input, expectedArray, output) => {
+function testEach(testCases: TestCases) {
+  test.each(testCases)(
+    "%s -> %s output = %s",
+    (input, expectedArray, output) => {
       expect(removeDuplicates(input)).toEqual(output);
       expect(input).toEqual(expectedArray);
-    });
+    }
+  );
+}
+
+type TestCases = [number[], number[], number][];
+
+describe("#removeDuplicates", () => {
+  describe("can return arrays of 1 length", () => {
+    const testCases: TestCases = [
+      [[1], [1], 0],
+      [[2], [2], 0],
+      [[1000], [1000], 0],
+      [[367899], [367899], 0],
+    ];
+
+    testEach(testCases);
+  });
+
+  describe("can return arrays of any length with no duplicates", () => {
+    const testCases: TestCases = [
+      [[1, 2, 3, 4], [1, 2, 3, 4], 0],
+      [[6, 8, 19, 52], [6, 8, 19, 52], 0],
+      [[-8, 0, 14, 98], [-8, 0, 14, 98], 0],
+    ];
+
+    testEach(testCases);
+  });
+
+  describe("can remove duplicates", () => {
+    const testCases: TestCases = [[[1, 1], [1], 1]];
+
+    testEach(testCases);
   });
 });
-
-type TestCases = [[number[], number[], number]];
--- src/lib/removeDuplicates.ts
+++ src/lib/removeDuplicates.ts
@@ -1,3 +1,16 @@
 export function removeDuplicates(nums: number[]): number {
-  return 0;
+  let dupesRemoved = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!nums[i + 1]) {
+      return dupesRemoved;
+    }
+
+    if (nums[i] == nums[i + 1]) {
+      dupesRemoved++;
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return dupesRemoved;
 }
