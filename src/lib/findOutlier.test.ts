c233b824365448b48114649188e437e76ab0fa67
Arshad Siddiqui <ferozarshad123@gmail.com>
2023-08-12 21:28:44 +0100

    Can find outlier in mostly odds or evens

--- src/lib/findOutlier.test.ts
+++ src/lib/findOutlier.test.ts
@@ -2,7 +2,10 @@ import { describe, test, expect } from "vitest";
 import { findOutlier } from "./findOutlier";
 
 describe("#findOutlier", () => {
-  const cases: Cases[] = [[[1, 2, 3], 2]];
+  const cases: Cases[] = [
+    [[1, 2, 3], 2],
+    [[2, 4, 5], 5],
+  ];
 
   test.each(cases)("%s -> %s", (input, output) => {
     expect(findOutlier(input)).toEqual(output);
--- src/lib/findOutlier.ts
+++ src/lib/findOutlier.ts
@@ -1,3 +1,24 @@
 export function findOutlier(integers: number[]): number {
+  let oddCount = 0;
+  let evenCount = 0;
+
+  integers.forEach((integer) => {
+    if (integer % 2 === 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+  });
+
+  let isEvenOutlier = evenCount > oddCount;
+
+  for (let i = 0; i < integers.length; i++) {
+    if (isEvenOutlier && integers[i] % 2 !== 0) {
+      return integers[i];
+    } else if (!isEvenOutlier && integers[i] % 2 === 0) {
+      return integers[i];
+    }
+  }
+
   return 2;
 }
